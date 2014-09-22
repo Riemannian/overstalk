b9779d745660cf7a03ab1716afec381e9d888ab3
X-C3LL <overloadblog@hotmail.es>
2014-09-22 08:10:14 +0200

    Added some SN more

--- lib/check.pl
+++ lib/check.pl
@@ -19,24 +19,48 @@ sub search_sn {
 		if ($trozo =~ m/https\:\/\/www\.facebook\.com\/(.*?)\"/g) { 
 			if ($trozo !~ /feed/) {$listado .= "[+] Facebook: $1#\n";}
 		}
+		if ($trozo =~ m/http\:\/\/www\.facebook\.com\/(.*?)\"/g) {
+                        if ($trozo !~ /feed/) {$listado .= "[+] Facebook: $1#\n";}
+                }
 		if ($trozo =~ m/https\:\/\/plus\.google\.com\/u\/0\/\+(.*?)\/po/g) {
 			$listado .= "[+] Google Plus: $1#\n";
 		}
-		if ($trozo =~ m/https\:\/\/twitter\.com\/(.*?)\"/g) {
+                if ($trozo =~ m/http\:\/\/plus\.google\.com\/u\/0\/\+(.*?)\/po/g) {
+                        $listado .= "[+] Google Plus: $1#\n";
+                }
+
+		if ($trozo =~ m/http\:\/\/twitter\.com\/(.*?)\"/g) {
 			if ($trozo !~ /tweet/) {$listado .= "[+] Twitter: $1#\n";}
 		}
+		if ($trozo =~ m/https\:\/\/twitter\.com\/(.*?)\"/g) {
+                        if ($trozo !~ /tweet/) {$listado .= "[+] Twitter: $1#\n";}
+                }
 		if ($trozo =~ m/http\:\/\/www\.linkedin\.com\/in\/(.*?)\"/g) {
 			$listado .= "[+] LinkedIn: $1#\n";
 		}
+                if ($trozo =~ m/https\:\/\/www\.linkedin\.com\/in\/(.*?)\"/g) {
+                        $listado .= "[+] LinkedIn: $1#\n";
+                }
+
 		if ($trozo =~ m/http\:\/\/instagram\.com\/(.*?)\"/g) {
 			$listado .= "[+] Instagram: $1#\n";
 		}
+                if ($trozo =~ m/https\:\/\/instagram\.com\/(.*?)\"/g) {
+                        $listado .= "[+] Instagram: $1#\n";
+                }
 		if ($trozo =~ m/http\:\/\/www\.pinterest\.com\/(.*?)\//g) {
 			$listado .= "[+] Pinteres: $1#\n";
 		}
+                if ($trozo =~ m/https\:\/\/www\.pinterest\.com\/(.*?)\//g) {
+                        $listado .= "[+] Pinteres: $1#\n";
+                }
 		if ($trozo =~ m/http\:\/\/www\.yelp\.es\/user_details\?userid\=(.*?)\"/g) {
 			$listado .= "[+] Yelp url ID: $1#\n";
 		}
+                if ($trozo =~ m/https\:\/\/www\.yelp\.es\/user_details\?userid\=(.*?)\"/g) {
+                        $listado .= "[+] Yelp url ID: $1#\n";
+                }
+
 		if ($trozo =~ m/http\:\/\/www\.lastfm\.es\/user\/(.*?)\"/g) {
 			$listado .= "[+] LastFM: $1#\n";
 		}
@@ -49,7 +73,24 @@ sub search_sn {
 		if ($trozo =~ m/http\:\/\/www\.flickr\.com\/(.*?)\"/g) {
                         $listado .= "[+] Flickr: $1#\n";
                 }
-		
+               if ($trozo =~ m/http\:\/\/www\.flickr\.com\/(.*?)\"/g) {
+                        $listado .= "[+] Flickr: $1#\n";
+                }
+		if ($trozo =~ m/flickr\.com\/photos\/(.*?)\"/g) {
+                        $listado .= "[+] Flickr: $1#\n";
+                }
+		if ($trozo =~ m/slideshare\.net\/(.*?)\"/g) {
+			print "[+] SlideShare: $1#\n";
+		}
+		if ($trozo =~ m/foursquare\.com\/user\/(.*?)\"/g) {
+			print "[+] FourSquare: $1#\n";
+		}
+		if ($trozo =~ m/youtube\.com\/user\/(.*?)\"/g) {
+			print "[+] Youtube: $1#\n";
+		}		
+		if ($trozo =~ m/xing\.com\/profile\/(.*?)\"/g) {
+			print "[+] Xing: $1#\n";
+		}
 	}
 	return $listado;
 }
